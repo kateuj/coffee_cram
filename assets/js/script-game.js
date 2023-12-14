8f456c348e01ede901fff1440f325dabca4e7e6c
kateuj <kate.ulloajames@gmail.com>
2023-12-14 12:22:24 +0000

    arrays for drink compositions created

--- assets/js/script-game.js
+++ assets/js/script-game.js
@@ -1,42 +1,105 @@
 const drinkNames = ["Mocha", "Latte", "Americano"];
-let drinkName = drinkNames[Math.floor(Math.random()*drinkNames.length)];
+var drinkName = drinkNames[Math.floor(Math.random()*drinkNames.length)];
 
-$( document ).ready(function() {
+// Drink name generation
+$(document).ready(function() {
     $("#drink-random").text(`${drinkName}`);
 });
 
-$("#reset-button").click(function() {
-    $("#drink-random").text("");
-    $("#drink-random").text(`${drinkName}`);
-});
+//Drink sequences
+const espresso = document.querySelector("#espresso-ingredient");
+const doubleEspresso = document.querySelector("#double-espresso-ingredient");
+const steamedMilk = document.querySelector("#milk-ingredient");
+const chocolate = document.querySelector("#chocolate-ingredient");
+const water = document.querySelector("#water-ingredient");
 
-// function ingredientClick () {
-//     if ()
-// }
-// function (){
-//     if (drinkName === "Mocha") {
+const americanoSequence = [
+    espresso,
+    water
+];
+
+const mochaSequence = [
+    doubleEspresso,
+    chocolate,
+    steamedMilk
+];
+
+const latteSequence = [
+    espresso,
+    chocolate,
+    steamedMilk
+];
+
+// if americano random drink name
+
+// if $(americanoSequence).click(espresso).then((
+//     $("#coffee-cup").attr('src', 'assets/images/single_espresso.png');
+//     $("#water-ingredient").click(function() {
+//         $("#coffee-cup").attr('src', 'assets/images/americano.png');
+//         setTimeout(500);
+//         location.reload();
+//         alert('Well done!');
+//     });
+    
+
+// )
+    
+// });
+
+const ingredientClicked = ingredientClicked => {
+    const expectedChoice = americanoSequence();
+    if (expectedChoice === ingredientClicked) {
+        if (americanoSequence.length === 0) {
+            //start new round
+            sequence.push(drinkName());
+        }
+    } else {
+        alert('game over');
+    }
+};
 
 
-// }
-// };
-$("#espresso-ingredient").click(function() {
-    $("#coffee-cup").attr('src', 'assets/images/single_espresso.png');
-});
-
+if (drinkName === "Americano") {
+    $("#espresso-ingredient").click(function() {
+        $("#coffee-cup").attr('src', 'assets/images/single_espresso.png');
+    });
+    $("#water-ingredient").click(function() {
+        $("#coffee-cup").attr('src', 'assets/images/americano.png');
+        setTimeout(500);
+        alert('Well done!');
+        location.reload();
+    });
+} else if (drinkName === "Mocha") {
+    $("#espresso-ingredient").click(function() {
+        $("#coffee-cup").attr('src', 'assets/images/single_espresso.png');
+    });
+    $("#chocolate-ingredient").click(function() {
+        $("#coffee-cup").attr('src', 'assets/images/chocolate.png');
+    });
+    $("#milk-ingredient").click(function() {
+        $("#coffee-cup").attr('src', 'assets/images/mocha.png');
+        alert('Well done!');
+        location.reload();
+    });
+} else if (drinkName === "Latte") {
+    $("#espresso-ingredient").click(function() {
+        $("#coffee-cup").attr('src', 'assets/images/single_espresso.png');
+    });
+    $("#milk-ingredient").click(function() {
+        $("#coffee-cup").attr('src', 'assets/images/mocha.png');
+        alert('Well done!');
+        location.reload();
+    });
+};
 
 // function showScore() {
 //     document.getElementById("score").innerText = game.score;
 // }
-// $(document).ready(function($) {
-//     $('#espresso-ingredient').on({
-//       'click': function() {
-//         $('#change-image').attr('src', 'https://cdn.shopify.com/s/files/1/0781/4423/files/02_8df18841-8d84-4a96-9611-e5a965dce73c.jpg?v=1568864598');
-//       }
-//     });
-  
-//     $('.yellow-button').on({
-//       'click': function() {
-//         $('#change-image').attr('src', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mothers-day-flower-bouquet-1588610191.jpg');
-//       }
-//     });
-//   });
+
+
+// Reset game
+$("#reset-button").click(function() {
+    $("#drink-random").text(`${drinkName}`);
+    location.reload();
+    
+});
