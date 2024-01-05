bc9464818496bcf5b0f9b513902fb3fd6e96cdc4
kateuj <kate.ulloajames@gmail.com>
2024-01-05 08:17:46 +0000

    add score and order count increases to playerWin function

--- assets/js/script-game.js
+++ assets/js/script-game.js
@@ -1,3 +1,13 @@
+// //Score and Order Count
+// let playerScore = 0;
+// let orderCount = 0;
+
+// //New game reset counts
+// function newGame() {
+//     orderCount = 0;
+//     playerScore = 0;
+// };
+
 var drinkNames = ['Mocha', 'Latte', 'Americano', 'Flat White', 'Single Espresso', 'Double Espresso', 'Cappuccino'];
 var drinkName = drinkNames[Math.floor(Math.random()*drinkNames.length)];
 
@@ -7,44 +17,47 @@ const steamedMilk = document.querySelector('#milk-ingredient');
 const chocolate = document.querySelector('#chocolate-ingredient');
 const water = document.querySelector('#water-ingredient');
 
+var divScore = document.querySelector("#player-score");
+var divOrderCount = document.querySelector("#order-count");
+
+var playerScore = divScore.textContent * 1 + 10;
+var orderCount = divOrderCount.textContent * 1 + 1;
+
 let americano = 'double espresso, water';
 let latte = 'double espresso, milk';
 let mocha = 'double espresso, chocolate, milk';
 let flatWhite = 'double espresso, milk';
 let cappuccino = 'espresso, milk, foam';
 
+//If player gets correct answer, runs below function
+function playerWin() {
+
+    divScore.textContent = playerScore;
+    divOrderCount.textContent = orderCount;
+    setTimeout(function(){
+        alert('Well done!');
+        userString = '';
+        location.reload('#drink-random');
+        //timer reset
+    },1000);
+};
+
+//add loop for 10 goes to restart with alert and text with score on
+
 // Drink name generation
 $(document).ready(function() {
     $('#drink-random').text(`${drinkName}`);
 });
 
-// setInterval(function () {
-//     var div = document.querySelector("#counter");
-//     var count = div.textContent * 1 - 1;
-//     div.textContent = count;
-//     if (count <= 0) {
-//         location.reload('#drink-random');
-//         // counter -10
-//     }
-//   }, 1000);
-
-// var divScore = document.querySelector("#score");
-// var count = divScore.textContent * 1 - 1;
-// $('#score').text(`${totalScore}`);
-  
+// Empty string generated for user ingredient clicks to make an ingredient list to check against the variable strings of each drink's mock up.
 let userString = '';
 
+// Click functions with if statements for each ingredient in the drawer. Each triggers the coffee cup image to change to reflect the user's choices or resets the level for incorrect answers.
 $('#espresso-ingredient').click(function() {
     userString += 'espresso';
     if (drinkName == 'Single Espresso' && userString == 'espresso') {
         $('#coffee-cup').attr('src', 'assets/images/espresso-cup.png');
-        setTimeout(function(){
-            alert('Well done!');
-        },1000);
-        setTimeout(function(){
-            userString = '';
-            location.reload('#drink-random');
-        },1000);
+        playerWin();
     } else if (drinkName == 'Cappuccino' && userString == 'espresso') {
         $('#coffee-cup').attr('src', 'assets/images/espresso-cup.png');
     } else {
@@ -58,13 +71,7 @@ $('#water-ingredient').click(function() {
     userString += ', water';
     if (drinkName == 'Americano' && userString == americano) {
         $('#coffee-cup').attr('src', 'assets/images/americano.png');
-        setTimeout(function(){
-            alert('Well done!');
-        },1000);
-        setTimeout(function(){
-            userString = '';
-            location.reload('#drink-random');
-        },1000);
+        playerWin();
     } else {
         alert('Oops! Try again!');
         userString = '';
@@ -87,13 +94,7 @@ $('#milk-ingredient').click(function() {
     userString += ', milk';
     if (drinkName == 'Latte' && userString == latte) {
         $('#coffee-cup').attr('src', 'assets/images/latte.png');
-        setTimeout(function(){
-            alert('Well done!');
-        },1000);
-        setTimeout(function(){
-            userString = '';
-            location.reload('#drink-random');
-        },1000);
+        playerWin();
     } else if (drinkName == 'Cappuccino' && userString == 'espresso, milk') {
         $('#coffee-cup').attr('src', 'assets/images/chocolate-cup.png');
     } else if (drinkName == 'Mocha' && userString == mocha) {
@@ -105,15 +106,10 @@ $('#milk-ingredient').click(function() {
             userString = '';
             location.reload('#drink-random');
         },1000);
+        playerWin();
     } else if (drinkName == 'Flat White' && userString == flatWhite) {
         $('#coffee-cup').attr('src', 'assets/images/latte.png');
-        setTimeout(function(){
-            alert('Well done!');
-        },1000);
-        setTimeout(function(){
-            userString = '';
-            location.reload('#drink-random');
-        },1000);
+        playerWin();
     } else {
         alert('Oops! Try again!');
         userString = '';
@@ -133,13 +129,7 @@ $('#double-espresso-ingredient').click(function() {
         $('#coffee-cup').attr('src', 'assets/images/espresso-cup.png');
     } else if (drinkName == 'Double Espresso' && userString == 'double espresso') {
         $('#coffee-cup').attr('src', 'assets/images/espresso-cup.png');
-        setTimeout(function(){
-            alert('Well done!');
-        },1000);
-        setTimeout(function(){
-            userString = '';
-            location.reload('#drink-random');
-        },1000);
+        playerWin();
     } else {
         alert('Oops! Try again!');
         userString = '';
@@ -151,13 +141,7 @@ $('#foam-ingredient').click(function() {
     userString += ', foam';
     if (drinkName == 'Cappuccino' && userString == cappuccino) {
         $('#coffee-cup').attr('src', 'assets/images/latte.png');
-        setTimeout(function(){
-            alert('Well done!');
-        },1000);
-        setTimeout(function(){
-            userString = '';
-            location.reload('#drink-random');
-        },1000);
+        playerWin();
     } else {
         alert('Oops! Try again!');
         userString = '';
@@ -166,30 +150,35 @@ $('#foam-ingredient').click(function() {
 });
 
 
+// Timer countdown for each question
+setInterval(function () {
+    var div = document.querySelector("#timer");
+    var count = div.textContent * 1 - 1;
+    div.textContent = count;
+    if (count <= 0) {
+        location.reload('#drink-random');
+        // counter -15
+    }
+  }, 1000);
 
-// $('#espresso-ingredient').click(function() {
-//     userString += 'espresso';
-//         if (drinkName == 'Americano' && userString == 'espresso') {
-//             $('#coffee-cup').attr('src', 'assets/images/espresso-cup.png');
-//         });
+//   updateScore(function () {
+//     var divScore = document.querySelector("#player-score");
+//     playerScore+=10;
+//     divScore.textContent = playerScore;
+
+// });
+
+
+
+// // Score
+// setScore(function () {
+//     var divScore = document.querySelector("#score");
+//     totalScore = divScore.textContent * 1 + 10;
+//     div.textContent = score;
+//     if ()
+//     $('#score').text(`${totalScore}`);
 // };
-
-// $('#water-ingredient').click(function() {
-//     userString += ', water';
-//     if (drinkName === 'Americano' && userString = 'espresso, water') {
-//         $('#coffee-cup').attr('src', 'assets/images/americano.png');
-//             setTimeout(function(){
-//             alert('Well done!');
-//             },1000);
-//             setTimeout(function(){
-//             location.reload();
-//             },1000);
-//             });
-// };
-
-// function showScore() {
-//     document.getElementById('score').innerText = game.score;
-// }
+// // Order count
 
 
 // Reset game
