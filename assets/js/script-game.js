29e7dc423681e85cde38616194b7cbe271c169c4
kateuj <kate.ulloajames@gmail.com>
2024-01-08 09:57:56 +0000

    score and order count id tag error change

--- assets/js/script-game.js
+++ assets/js/script-game.js
@@ -1,13 +1,3 @@
-// //Score and Order Count
-// let playerScore = 0;
-// let orderCount = 0;
-
-// //New game reset counts
-// function newGame() {
-//     orderCount = 0;
-//     playerScore = 0;
-// };
-
 var drinkNames = ['Mocha', 'Latte', 'Americano', 'Flat White', 'Single Espresso', 'Double Espresso', 'Cappuccino'];
 var drinkName = drinkNames[Math.floor(Math.random()*drinkNames.length)];
 
@@ -20,27 +10,11 @@ const water = document.querySelector('#water-ingredient');
 var divScore = document.querySelector("#player-score");
 var divOrderCount = document.querySelector("#order-count");
 
-var playerScore = divScore.textContent * 1 + 10;
-var orderCount = divOrderCount.textContent * 1 + 1;
-
-let americano = 'double espresso, water';
-let latte = 'double espresso, milk';
-let mocha = 'double espresso, chocolate, milk';
-let flatWhite = 'double espresso, milk';
-let cappuccino = 'espresso, milk, foam';
-
-//If player gets correct answer, runs below function
-function playerWin() {
-
-    divScore.textContent = playerScore;
-    divOrderCount.textContent = orderCount;
-    setTimeout(function(){
-        alert('Well done!');
-        userString = '';
-        location.reload('#drink-random');
-        //timer reset
-    },1000);
-};
+var americano = 'double espresso, water';
+var latte = 'double espresso, milk';
+var mocha = 'double espresso, chocolate, milk';
+var flatWhite = 'double espresso, milk';
+var cappuccino = 'espresso, milk, foam';
 
 //add loop for 10 goes to restart with alert and text with score on
 
@@ -51,6 +25,21 @@ $(document).ready(function() {
 
 // Empty string generated for user ingredient clicks to make an ingredient list to check against the variable strings of each drink's mock up.
 let userString = '';
+
+let playerScore = divScore.textContent * 1 + 10;
+let orderCount = divOrderCount.textContent * 1 + 1;
+
+//If player gets correct answer, runs below function
+function playerWin() {
+    divScore.textContent = playerScore;
+    divOrderCount.textContent = orderCount;
+    setTimeout(function(){
+        alert('Well done!');
+        userString = '';
+        location.reload('#drink-random');
+        //timer reset
+    },1000);
+};
 
 // Click functions with if statements for each ingredient in the drawer. Each triggers the coffee cup image to change to reflect the user's choices or resets the level for incorrect answers.
 $('#espresso-ingredient').click(function() {
